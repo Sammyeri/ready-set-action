c3752ef12876cc027178163741bebd34c06196f2
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2022-07-27 08:04:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function main(){
+   const grow = document.getElementById("grow-me");
+   grow.classList.add("big");
+   const shrink = document.getElementById("shrink-me");
+   shrink.classList.remove("big");
+   const list = document.querySelectorAll("li");
+    list.forEach(element => {console.log(element.outerText)});
+    const link = document.querySelector("a");
+    link.innerText = ("somwhere");
+    link.setAttribute("href", "https://www.example.com");
+    const hide = document.getElementById("hide-me");
+    hide.style.display = "none";
+    const show = document.getElementById("show-me");
+    show.style.display = "block";
+    const inputName = document.getElementById("name").value;
+    console.log(inputName);
+}
